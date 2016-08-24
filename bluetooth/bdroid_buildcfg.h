dfe6a88090894ffd5f9ea866275c64d35bdfc2d8
Chirayu Desai <chirayudesai1@gmail.com>
2016-08-24 18:35:07 +0530

    bluetooth: Our kernel is missing CLOCK_BOOTTIME_ALARM (alarmtimer)
    * Kernel is still using drivers/rtc/alarm.c, instead of the newer
      kernel/time/alarmtimer.c
    
    Change-Id: I2b8635fa887b2cfc0fca820fb560539475124534

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -20,4 +20,7 @@
 
 #define BTM_DEF_LOCAL_NAME   "Samsung Galaxy Tab Pro 8.4"
 
+/* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
+#define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
+
 #endif
