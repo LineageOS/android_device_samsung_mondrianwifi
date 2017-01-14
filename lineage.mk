772a20c450d27429eb5fd9adbfe4fbc87dab12fa
Christopher R. Palmer <crpalmer@gmail.com>
2017-01-14 06:15:41 -0500

    mondrianwifi: Rebrand cm -> lineage
    Change-Id: I50bd245518253061b4bba9f93195202bf4da7a33

--- lineage.mk
+++ lineage.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/samsung/mondrianwifi/full_mondrianwifi.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 PRODUCT_DEVICE := mondrianwifi
-PRODUCT_NAME := cm_mondrianwifi
+PRODUCT_NAME := lineage_mondrianwifi
